ca06b82710cae22633c1cb649e82df821d6a7778
oliperraul <olivier.perrault@usherbrooke.ca>
2019-05-11 03:41:42 -0400

    asd

--- README.md
+++ README.md
@@ -3,14 +3,13 @@ https://www.boost.org/users/download/
 How to install boost
 https://www.ibm.com/support/knowledgecenter/en/SSWTQQ_1.0.0/com.ibm.swg.ba.cognos.trade_analytics.1.0.0.doc/t_trd_installboost.html
 
-
 * Set this environment variables:
 
 BOOST_ROOT
 C:\boost_1_70_0
 
-
-Use Visual Studio 2017
-
-* CMake
+* CMake in visual studio tutorial (Visual Studio 2017)
 https://www.youtube.com/watch?v=_lKxJjV8r3Y
+
+
+* See program_option for an example of how I add a dependency
--- gitus/gitus.cpp
+++ gitus/gitus.cpp
@@ -2,8 +2,71 @@
 
 #include <dummy.h>
 
-int main()
+#include <iostream>
+#include <boost/program_options.hpp>
+
+#include <dummy.h>
+
+// TODO: use boost::program_options::value<int>() for args
+void init(bool res)
 {
-    std::cout << GetAnswerToLifeUniverseAndEverything() << "\n";
-    return 0;
+	std::cout << "INIT" << std::endl;
+	int x;
+	std::cin >> x;
+}
+
+void commit(bool res)
+{
+	std::cout << "DO IT" << std::endl;
+	int x;
+	std::cin >> x;
+}
+
+void add(bool res)
+{
+	std::cout << "ADD" << std::endl;
+	int x;
+	std::cin >> x;
+}
+
+
+int main(int argc, char **argv)
+{
+	using namespace boost::program_options;
+
+	try
+	{
+		std::cout << argv[1] << std::endl;
+
+		options_description description("Allowed options");
+		variables_map options;
+		store(parse_command_line(argc, argv, description), options);
+
+		if (options.count("init"))
+		{
+			option_description d;
+			///*description*/.add("init", boost::program_options::bool_switch()->notifier(&init));
+		}
+
+		if (description.options().empty())
+		{
+			description.add_options()
+				("help,h", "Help screen")
+				("init", boost::program_options::bool_switch()->notifier(&init))
+				("commit", boost::program_options::bool_switch()->notifier(&commit));
+			//("add", boost::program_options::bool_switch()->notifier(&add));
+		}
+
+
+		options.notify();
+		//boost::program_options::notify(options);
+
+	}
+	catch (std::exception & ex)
+	{
+		std::cerr << "Error: " << ex.what() << std::endl;
+		return 1;
+	}
+	
+	return 0;
 }
