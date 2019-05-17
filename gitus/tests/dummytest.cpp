5877e6a9251f7cd13c0f97784f772d57c85a7d7c
will <wgingras94@gmail.com>
2019-05-17 10:08:20 -0400

    fix unit tests to behave correctly

--- gitus/tests/dummytest.cpp
+++ gitus/tests/dummytest.cpp
@@ -31,16 +31,21 @@ BOOST_AUTO_TEST_CASE(InitNormal)
 	auto objDirExist = boost::filesystem::exists(objDir);
 	auto refDirExist = boost::filesystem::exists(refDir);
 	auto headDirExist = boost::filesystem::exists(headDir);
+	auto headFileExist = boost::filesystem::exists(headFile);
+	auto masterFileExist = boost::filesystem::exists(masterFile);
+
 	auto headFileSize = boost::filesystem::file_size(headFile);
-	auto masterFileSize = boost::filesystem::file_size(masterFile);
+	auto masterFileSize = boost::filesystem::file_size(headFile);
 
 	BOOST_CHECK(gitusDirExist);
 	BOOST_CHECK(objDirExist);
 	BOOST_CHECK(refDirExist);
 	BOOST_CHECK(headDirExist);
+	BOOST_CHECK(headFileExist);
+	BOOST_CHECK(masterFileExist);
 	BOOST_CHECK_EQUAL(headFileSize, 0);
 	BOOST_CHECK_EQUAL(masterFileSize, 0);
-	
+
 	CleanUp();
 }
 
@@ -75,6 +80,9 @@ BOOST_AUTO_TEST_CASE(Add)
 	//Assert
 	auto fileObjectCreated = boost::filesystem::exists(filePath);
 	BOOST_CHECK(fileObjectCreated);
+
+	CleanUp();
+	DeleteFile(fileName);
 }
 
 /*
@@ -82,7 +90,7 @@ BOOST_AUTO_TEST_CASE(Add)
 init
  -chacun de repo sont crer
  -init dans un deja init
-
+ {m_pathname=L"C:\\Users\\zeroeagle1\\CMakeBuilds\\9ea20e7e-e12c-ff34-9a63-2204612524e4\\build\\x64-Debug\\tests\\.git/objects/e2\\b64f6bf69d2916af31f293da31c10d6a575a0b" }
 add
  -/ob/xx/xxxxxxxx bien crer
  - index bien crer
@@ -114,14 +122,16 @@ void DeleteFile(std::string fileName) {
 }
 
 void CreateFile(std::string fileName, std::string content) {
-	auto ofs = boost::filesystem::ofstream(fileName);
+	boost::filesystem::ofstream ofs{ fileName };
 	ofs << content + "\0";
 }
 
-boost::filesystem::path GetFileObjPath(std::string filePath) {
-	auto fileHash = GitusUtils::HashObject(filePath, GitusUtils::Blob, false);
+boost::filesystem::path GetFileObjPath(std::string fileName) {
+	auto fileContent = GitusUtils::ReadFile(fileName);
+	auto fileHash = GitusUtils::HashObject(fileContent, GitusUtils::Blob, false);
 	auto fileDirHead = fileHash.substr(0, 2);
-	auto fileDirBody = fileHash.substr(2);
+	auto fileDirBody = fileHash.substr(2, std::string::npos);
 	auto objDir = GitusUtils::ObjectsDirectory();
 	auto filePath = objDir / fileDirHead / fileDirBody;
+	return filePath;
 }
